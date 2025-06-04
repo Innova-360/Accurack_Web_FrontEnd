af4fe5ec4957ce8b8a4e8e707a586169ef6dfd89
= <=>
2025-06-04 21:36:09 +0500

    fixed bugs

---
+++ src/components/LandingPageComponents/SeamlessIntegration.tsx
@@ -0,0 +1,18 @@
+
+
+function SeamlessIntegration() {
+  return (
+    <>
+    <div className='flex flex-col items-center justify-center mt-20 bg-[var(--primary-color)] overflow-x-hidden'>
+        <div className="mt-20 text-4xl md:text-5xl lg:text-5xl font-bold text-[#f9fafd] fonts text-center gap-1.5 flex flex-col">
+            <div className=''>Seamless POS <span className="text-[#50BED2]">Integration</span></div>
+            {/* <img src="/underlineLight.svg" className="md:ml-96 lg:ml:96 m-auto" alt="underline" /> */}
+        </div>
+        <div className='w-[110%] lg:w-[85%] xl:w-[85%] mt-20 px-4'><img src="/phone.png" alt="phone" /></div>
+    </div>
+    <div className='relative md:h-[9rem] lg:h-[9rem] h-[6rem] -z-10 bottom-14 md:bottom-64 lg:bottom-64'><img src="/lines.svg" alt="lines" /></div>
+    </>
+  )
+}
+
+export default SeamlessIntegration
---
+++ src/components/LandingPageComponents/Tagline.tsx
@@ -0,0 +1,13 @@
+function Tagline() {
+  return (
+    <div className='bg-[#d0f1f8] h-8 md:mt-20 lg:mt-20 mt-7 overflow-hidden'>
+      <div className='animate-marquee whitespace-nowrap text-[var(--primary-color)] text-sm bg-[#d0f1f8] flex items-center w-max'>
+        <span className="mx-24">Accurack – Precision in Every Stock Count.</span>
+        <span className="mx-24">Smarter Inventory, Stronger Business.</span>
+        <span className="mx-24">Track Better. Stock Smarter. Grow Faster.</span>
+      </div>
+    </div>
+  );
+}
+
+export default Tagline;
